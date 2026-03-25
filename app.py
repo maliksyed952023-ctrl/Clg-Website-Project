e2a4556bf095c7fe1acf71dbbc97c3df01b1932c
abhi-ghub-2007 <abhiramkhardekar@gmail.com>
2026-03-25 14:30:44 +0530

    Manually pushing all missing folders and files

--- app.py
+++ app.py
@@ -131,3 +131,8 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
